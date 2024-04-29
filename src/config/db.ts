e8c0eb5c23963bae86081c6e4824a2a232e38bf1
Ana Rius <a.riusvi11@gmail.com>
2024-04-29 15:26:26 +0200

    feat: add migrations for creating roles table & scripts to run-migrations and revert-migrations

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,7 @@
 import "reflect-metadata";
+import 'dotenv/config';
 import { DataSource } from "typeorm";
+import { CreateRoles1714396075567 } from "../database/migrations/1714396075567-create_roles";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
@@ -9,6 +11,7 @@ export const AppDataSource = new DataSource({
     password: process.env.DB_PASSWORD || "1234",
     database: process.env.DB_DATABASE || "ichigo_plushies_DB",
     entities: [],
+    migrations: [CreateRoles1714396075567],
     synchronize: false,
     logging: false,
 });
---
+++ src/database/migrations/1714396075567-create_roles.ts
@@ -0,0 +1,46 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateRoles1714396075567 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "roles",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+                        length: "255",
+                    },
+                    {
+                        name: "description",
+                        type: "text",
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                ],
+            }),
+            true
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("roles");
+    }
+
+}
