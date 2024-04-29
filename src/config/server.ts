0cc2b1bdcc1f6d2f41d9306c1907f772c9682215
Ana Rius <a.riusvi11@gmail.com>
2024-04-29 11:57:21 +0200

    feat: dotenv library installed

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,10 +1,17 @@
 import { AppDataSource } from "./db";
 import express, { Application } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app: Application = express();
 
-app.listen(4000,() => {
-    console.log("Server is running on port 4000");
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT,() => {
+    console.log("=================================\n",
+                `Server is running on port: ${PORT}\n`,
+                "=================================");
 });
 
 // AppDataSource.initialize().then(() => {
