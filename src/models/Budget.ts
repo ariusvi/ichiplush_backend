f3b5d9cf7a42be118b6ff84ed94f33c60b41735f
Ana Rius <a.riusvi11@gmail.com>
2024-04-30 12:59:00 +0200

    Feat: Budget model created

---
+++ src/models/Budget.ts
@@ -0,0 +1,42 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, Timestamp } from "typeorm"
+
+@Entity('budget')
+export class Budget extends BaseEntity {
+
+    @PrimaryGeneratedColumn()
+    id!: number
+
+    @Column({name:"reference"})
+    reference!: string
+
+    @Column({name:"email"})
+    email!: string
+
+    @Column({name:"quantity"})
+    quantity!: number
+
+    @Column({name:"base_body"})
+    baseBody!: number
+
+    @Column({name:"without_face"})
+    withoutFace!: boolean
+
+    @Column({name:"animal_ears"})
+    animalEars!: boolean
+
+    @Column({name:"wings"})
+    wings!: boolean
+
+    @Column({name:"total_price"})
+    totalPrice!: number
+
+    @Column({name: "is_active"})
+    isActive!: boolean
+
+    @Column({name: "created_at"})
+    createdAt!: Timestamp
+
+    @Column({name: "updated_at"})
+    updatedAt!: Timestamp
+
+}
