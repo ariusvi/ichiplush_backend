c44669489b2198c09aedbb9c547f3c8ad43df554
Ana Rius <a.riusvi11@gmail.com>
2024-05-02 19:12:26 +0200

    feat: add initial user data to data.sql

--- src/database/sql/data.sql
+++ src/database/sql/data.sql
@@ -8,4 +8,11 @@ insert into roles (id, name, description) values (3, 'crafter', 'The person who 
 insert into roles (id, name) values (4, 'user');
 insert into roles (id, name, description) values (5, 'subscriber', 'Once per month will recieve a plushie accessory');
 
+-- create users in system
+insert into users (id, user_name, role_id, password, email, is_active) values (1, 'super_admin', 1, '$08$cISKK0HLqPO/bpG5jdMtpuaghTzDqkGv1FkdzJERbmFLZaTtJ4LNW', 'superadmin@email.com', 1);
+insert into users (id, user_name, role_id, password, email, is_active) values (2, 'admin', 2, '$08$cISKK0HLqPO/bpG5jdMtpuaghTzDqkGv1FkdzJERbmFLZaTtJ4LNW', 'admin@email.com', 1);
+insert into users (id, user_name, role_id, password, email, is_active) values (3, 'crafter', 3, '$08$cISKK0HLqPO/bpG5jdMtpuaghTzDqkGv1FkdzJERbmFLZaTtJ4LNW', 'crafter@crafter.com', 1);
+insert into users (id, user_name, role_id, password, email, is_active) values (4, 'user', 4, '$08$cISKK0HLqPO/bpG5jdMtpuaghTzDqkGv1FkdzJERbmFLZaTtJ4LNW', 'user@email.com', 1);
+insert into users (id, user_name, role_id, password, email, is_active) values (5, 'user_subscriber', 5, '$08$cISKK0HLqPO/bpG5jdMtpuaghTzDqkGv1FkdzJERbmFLZaTtJ4LNW', 'subscriber@email.com', 1);
+
 -- pashword_hash $08$cISKK0HLqPO/bpG5jdMtpuaghTzDqkGv1FkdzJERbmFLZaTtJ4LNW -> @Test12345
