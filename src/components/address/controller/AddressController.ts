0800302ac1a1b0c602d4624e257e97f3c964c454
Ana Rius <a.riusvi11@gmail.com>
2024-05-03 17:41:47 +0200

    feat: Add updateAddress route to addressController.ts and addressRoutes.ts

--- src/components/address/controller/AddressController.ts
+++ src/components/address/controller/AddressController.ts
@@ -73,3 +73,67 @@ export const getAddresses = async (req: Request, res: Response) => {
         })
     }
 }
+
+export const updateAddress = async (req: Request, res: Response) => {
+
+    try {
+        const userId = req.tokenData.id
+        const addressId = req.body.id
+        const title = req.body.title
+        const name = req.body.name
+        const surname = req.body.surname
+        const phone = req.body.phone
+        const street = req.body.street
+        const city = req.body.city
+        const state = req.body.state
+        const country = req.body.country
+        const postalCode = req.body.postalCode
+
+        // check if all required fields are filled
+        if (!title || !name || !surname || !phone || !street || !city || !state || !country || !postalCode) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            })
+        }
+
+        const address = await Address.findOne({
+            where: {
+                id: addressId,
+                userId: userId
+            }
+        })
+
+        if (!address) {
+            return res.status(404).json({
+                success: false,
+                message: "Address not found"
+            })
+        }
+
+        address.title = title
+        address.name = name
+        address.surname = surname
+        address.phone = phone
+        address.street = street
+        address.city = city
+        address.state = state
+        address.country = country
+        address.postalCode = postalCode
+
+        const addressUpdated = await address.save()
+
+        res.status(200).json({
+            success: true,
+            message: "Address updated successfully",
+            data: addressUpdated
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Address can't be updated",
+            error: error
+        })
+    }
+}
--- src/components/address/routes/addressRoutes.ts
+++ src/components/address/routes/addressRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createAddress, getAddresses } from '../controller/AddressController';
+import { createAddress, getAddresses, updateAddress } from '../controller/AddressController';
 import { auth } from '../../../middlewares/authMiddleware';
 const router = express.Router();
 
 router.post('/create', auth, createAddress)
 router.get('/', auth, getAddresses)
+router.put('/', auth, updateAddress)
 
 export default router;
