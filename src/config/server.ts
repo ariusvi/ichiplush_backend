918796e066d6971e348cf7e6973d0852378a68b8
Ana Rius <a.riusvi11@gmail.com>
2024-05-06 13:58:34 +0200

    feat: cors installed

--- src/config/server.ts
+++ src/config/server.ts
@@ -2,10 +2,13 @@ import { AppDataSource } from "./db";
 import express from "express";
 import dotenv from "dotenv";
 import router from "../routes/router";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use('/api', router)
